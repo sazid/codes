de0d42a12deeb4b55bec46f41792a78cc981ecd2
sazid <sazidozon@gmail.com>
2017-08-11 13:11:43 +0600

    Max subarray sum algo1

--- problem_solving/handbook/max_subarray_sum.cpp
+++ problem_solving/handbook/max_subarray_sum.cpp
@@ -3,14 +3,20 @@ using namespace std;
 
 int main() {
 	int array[] = {-1, 2, 4, -3, 5, 2, -5, 2};
-
 	int n = sizeof(array) / sizeof(int);
 	int best = 0;
+
 	for (int a = 0; a < n; a++) {
 		for (int b = a; b < n; b++) {
-			
+			int sum = 0;
+			for (int k = a; k <= b; k++) {
+				sum += array[k];
+			}
+			best = max(best, sum);
 		}
 	}
+
+	cout << best << "\n";
 
 	return 0;
 }
