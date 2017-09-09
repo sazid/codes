16a2f2525eb93c14b560929bbb81881ecf69a394
sazid <sazidozon@gmail.com>
2017-09-10 05:58:18 +0600

    .

--- problem_solving/vjudge/2017_09_08/C.cpp
+++ problem_solving/vjudge/2017_09_08/C.cpp
@@ -3,51 +3,54 @@
 using namespace std;
 
 typedef unsigned long ul;
-pair <bool, vector <unsigned long> > NUM[5*1000001];
+pair <bool, unsigned long> NUM[5*1000001];
 
 void seive_primes(ul n) {
   NUM[0].first = 1;
   NUM[1].first = 1;
 
+  cout << NUM[0].second << endl;
+
   for (ul i=2; i < n; i++) {
+    NUM[i].second = 1;
     if (NUM[i].first == 0) {
-      NUM[i].second.PB(i);
+      NUM[i].second = i;
 
       for (ul p=i+i; p < n; p+=i) {
         NUM[p].first = 1;
-        NUM[p].second.PB(i);
+        NUM[p].second *= i;
       }
     }
   }
 }
 
-ul score(ul n) {
-  ul x=n;
-  for (ul i=0; i < NUM[n].second.size(); i++) {
-    x *= 1.0 - (1.0/NUM[n].second[i]);
-  }
+// ul score(ul n) {
+//   ul x=n;
+//   for (ul i=0; i < NUM[n].second.size(); i++) {
+//     x *= 1.0 - (1.0/NUM[n].second[i]);
+//   }
 
-  return x*x;
-}
+//   return x*x;
+// }
 
-void calculate_scores(ul n) {
-  NUM[0].second.PB(0);
-  for (ul i = 1; i <= n; i++) {
-    NUM[i].second.PB( NUM[i-1].second.back() + score(i) );
-  }
-}
+// void calculate_scores(ul n) {
+//   NUM[0].second.PB(0);
+//   for (ul i = 1; i <= n; i++) {
+//     NUM[i].second.PB( NUM[i-1].second.back() + score(i) );
+//   }
+// }
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   seive_primes(5*1000001);
-  calculate_scores(5*1000001);
+  // calculate_scores(5*1000001);
 
   unsigned long T, i=1, a, b;
   cin >> T;
   while (T-- > 0) {
     cin >> a >> b;
-    cout << "Case " << i++ << ": " << (NUM[b].second.back() - NUM[a-1].second.back()) << endl; 
+    // cout << "Case " << i++ << ": " << (NUM[b].second.back() - NUM[a-1].second.back()) << endl; 
   }
   
   return 0;
