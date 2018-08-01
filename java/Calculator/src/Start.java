ad0fcc9925f5831d7686ac032e24b92b08a20da9
sazid <sazidozon@gmail.com>
2018-08-01 14:48:32 +0600

    Move all resource files to res folder and make compatible with Java versions less than 8
    Signed-off-by: sazid <sazidozon@gmail.com>

--- java/Calculator/src/Start.java
+++ java/Calculator/src/Start.java
@@ -2,26 +2,14 @@ import java.awt.*;
 
 public class Start {
     public static void main(String[] args) {
-        // NOTE: using lambda instead of anonymous classes (requires java 8)
         // run the GUI from a different (EDT) thread so that calculations
         // on the main thread do not *halt* the GUI thread
-        EventQueue.invokeLater(() -> {
-            Calculator calc = new Calculator();
-            calc.setVisible(true);
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Calculator calc = new Calculator();
+                calc.setVisible(true);
+            }
         });
-
-//        EquationParser parser = new EquationParser();
-//        parser.equation.add("23");
-//        parser.equation.add("*");
-//        parser.equation.add("-4");
-//        parser.equation.add("-");
-//        parser.equation.add("89.32");
-//        parser.equation.add("-");
-//        parser.equation.add("0.012");
-//        parser.equation.add("/");
-//        parser.equation.add("1");
-//        System.out.println(parser.validate(parser.equation));
-//
-//        System.out.println(parser.solve(parser.equation));
     }
 }
